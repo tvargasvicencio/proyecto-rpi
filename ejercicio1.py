93c8a38cc0179af333b53c04627d7dfa1affa4ed
Tomas Vargas <tvargas.vicencio@gmail.com>
2018-07-05 06:29:52 +0000

    Arreglos ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -41,11 +41,11 @@ if __name__ == '__main__':
                 resultado = distancia.detectarObstaculo(sensor["Trig"],sensor["Echo"])
                 if resultado["medida"] <=5:
                     elapsed_time = time.time() - start_time
-                    print "tiempo de reaccion: %" %elapsed_time
+                    print "tiempo de reaccion: " + str(elapsed_time)
                     print "%.2f" %resultado["medida"] #por ultimo, vamos a mostrar el resultado por pantalla
                     led.apagar(sensor["led"])
                     midiendo = False
-                    time.sleep(randint(0,5))
+                    time.sleep(randint(0,3))
     except KeyboardInterrupt:
         print('LEDS OFF...\n')
         GPIO.output(sensores[0]["led"],GPIO.LOW)
