18c9ef3f91654bcc6653d927e8837a468c660cf6
Tomas Vargas <tvargas.vicencio@gmail.com>
2018-07-05 05:33:18 +0000

    Cambios en ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -21,16 +21,17 @@ US = {
     }
 }
 
-led.setup(LEDS.led1)
-led.setup(LEDS.led2)
-distancia.setup(US.us1.Trig,US.us1.Echo)
-distancia.setup(US.us2.Trig,US.us2.Echo)
+led.setup(LEDS["led1"])
+led.setup(LEDS["led2"])
+distancia.setup(US["us1"]["Trig"],US["us1"]["Echo"])
+distancia.setup(US["us2"]["Trig"],US["us2"]["Echo"])
 
 if __name__ == '__main__':
     try:
-        led.main(LEDS.led2)
+        led.main(LEDS["led2"])
     except KeyboardInterrupt:
         print('LED OFF...\n')
-        GPIO.output(LEDS.led1,GPIO.LOW)
-        GPIO.output(LEDS.led2,GPIO.LOW)
-        led.destroy(LEDS.led2)
+        GPIO.output(LEDS["led1"],GPIO.LOW)
+        GPIO.output(LEDS["led2"],GPIO.LOW)
+	led.destroy(LEDS["led1"])
+        led.destroy(LEDS["led2"])
