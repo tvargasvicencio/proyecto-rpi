d5b8d1defb03818a38418fb6dadeb5044e79699c
Tomas Vargas Vicencio <tvargas.vicencio@gmail.com>
2018-07-05 02:09:12 -0400

    fix distancia

--- sensores/distancia.py
+++ sensores/distancia.py
@@ -13,29 +13,27 @@ def setup(Trig,Echo):
 #Para leer la distancia del sensor al objeto, creamos una funcion
 def detectarObstaculo(Trig,Echo):
 
-   GPIO.output(Trig, False) #apagamos el pin Trig
-   time.sleep(2*10**-6) #esperamos dos microsegundos
-   GPIO.output(Trig, True) #encendemos el pin Trig
-   time.sleep(10*10**-6) #esperamos diez microsegundos
-   GPIO.output(Trig, False) #y lo volvemos a apagar
+    GPIO.output(Trig, False) #apagamos el pin Trig
+    time.sleep(2*10**-6) #esperamos dos microsegundos
+    GPIO.output(Trig, True) #encendemos el pin Trig
+    time.sleep(10*10**-6) #esperamos diez microsegundos
+    GPIO.output(Trig, False) #y lo volvemos a apagar
 
-  #empezaremos a contar el tiempo cuando el pin Echo se encienda
-   while GPIO.input(Echo) == 0:
-      start = time.time()
+    #empezaremos a contar el tiempo cuando el pin Echo se encienda
+    while GPIO.input(Echo) == 0:
+        start = time.time()
+    while GPIO.input(Echo) == 1:
+        end = time.time()
 
-   while GPIO.input(Echo) == 1:
-      end = time.time()
+    #La duracion del pulso del pin Echo sera la diferencia entre
+    #el tiempo de inicio y el final
+    duracion = end-start
 
-   #La duracion del pulso del pin Echo sera la diferencia entre
-   #el tiempo de inicio y el final
-   duracion = end-start
-
-   #Este tiempo viene dado en segundos. Si lo pasamos
-   #a microsegundos, podemos aplicar directamente las formulas
-   #de la documentacion
-   duracion_micro = duracion*10**6
-   medida = duracion_micro/58 #hay que dividir por la constante que pone en la documentacion, nos dara la distancia en cm
-   
+    #Este tiempo viene dado en segundos. Si lo pasamos
+    #a microsegundos, podemos aplicar directamente las formulas
+    #de la documentacion
+    duracion_micro = duracion*10**6
+    medida = duracion_micro/58 #hay que dividir por la constante que pone en la documentacion, nos dara la distancia en cm
     return {"duracion":duracion,"medida":medida}
 
 def destroy(Trig,Echo):
