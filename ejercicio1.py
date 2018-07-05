7c782d49a53ae8a08314b8fee2db051d580804b1
Tomas Vargas Vicencio <tvargas.vicencio@gmail.com>
2018-07-05 01:50:02 -0400

    cambios en ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,32 +6,31 @@ GPIO.setwarnings(False)
 #set the gpio modes to board numbering
 GPIO.setmode(GPIO.BOARD)
 
-LEDS = {
-    "led1": 7,
-    "led2": 12
-}
-US = {
-    "us1":{
-        "Trig": 11,
-        "Echo": 13
+sensores = [
+    {
+    "led": 7,
+    "Trig": 11,
+    "Echo": 13
     },
-    "us2":{
-        "Trig": 16,
-        "Echo": 18
-    }
-}
+    {
+    "led": 12,
+    "Trig": 16,
+    "Echo": 18
+    },
 
-led.setup(LEDS["led1"])
-led.setup(LEDS["led2"])
-distancia.setup(US["us1"]["Trig"],US["us1"]["Echo"])
-distancia.setup(US["us2"]["Trig"],US["us2"]["Echo"])
+]
+
+led.setup(sensores[0]["led"])
+led.setup(sensores[1]["led"])
+distancia.setup(sensores[0]["Trig"],sensores[0]["Echo"])
+distancia.setup(sensores[1]["Trig"],sensores[1]["Echo"])
 
 if __name__ == '__main__':
     try:
-        led.main(LEDS["led2"])
+        led.main(sensores[0]["led"])
     except KeyboardInterrupt:
-        print('LED OFF...\n')
-        GPIO.output(LEDS["led1"],GPIO.LOW)
-        GPIO.output(LEDS["led2"],GPIO.LOW)
-	led.destroy(LEDS["led1"])
-        led.destroy(LEDS["led2"])
+        print('LEDS OFF...\n')
+        GPIO.output(sensores[0]["led"],GPIO.LOW)
+        GPIO.output(sensores[1]["led"],GPIO.LOW)
+	    led.destroy(sensores[0]["led"])
+        led.destroy(sensores[1]["led"])
