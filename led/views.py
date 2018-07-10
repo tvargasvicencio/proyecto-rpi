1cafd70a3ad0baa5a5d89711c3aee5497a06052b
Tomas Vargas <tvargas.vicencio@gmail.com>
2018-07-10 06:52:37 +0000

    Configurando urls y arreglando codigo de prueba

--- led/views.py
+++ led/views.py
@@ -8,14 +8,14 @@ import RPi.GPIO as GPIO
 
 LED_PIN = 7
 
-def turnOn(request) {
+def turnOn(request):
     GPIO.setmode(GPIO.BOARD)
     GPIO.output(LED_PIN, 1)
     return HttpResponse('')
-}
 
-def turnOff(request) {
+
+def turnOff(request):
     GPIO.setmode(GPIO.BOARD)
     GPIO.output(LED_PIN, 0)
-    Return HttpResponse('')
-}
+    return HttpResponse('')
+
