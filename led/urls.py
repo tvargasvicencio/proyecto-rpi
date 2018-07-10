745f816c4323003ec15ad280f516f09dc6a11745
Tomas Vargas <tvargas.vicencio@gmail.com>
2018-07-10 07:04:01 +0000

    Prueba led por http funcionando

--- led/urls.py
+++ led/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.turnOn, name='turnOn'),
-    url(r'^$', views.turnOff, name='turnOff'),
+    url(r'^turnOn$', views.turnOn, name='turnOn'),
+    url(r'^turnOff$', views.turnOff, name='turnOff'),
 ]
--- led/views.py
+++ led/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 import RPi.GPIO as GPIO
@@ -10,14 +11,14 @@ LED_PIN = 7
 
 def turnOn(request):
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(LEDPIN,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(LED_PIN,GPIO.OUT,initial=GPIO.LOW)
     GPIO.output(LED_PIN, 1)
     return HttpResponse('')
 
 
 def turnOff(request):
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(LEDPIN,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(LED_PIN,GPIO.OUT,initial=GPIO.LOW)
     GPIO.output(LED_PIN, 0)
     return HttpResponse('')
 
