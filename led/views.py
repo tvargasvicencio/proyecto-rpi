792bb9fc8538047a4882ba67d6d2234b909343dd
Tomas Vargas Vicencio <tvargas.vicencio@gmail.com>
2018-07-10 02:40:08 -0400

    Prueba encender leds por url

--- led/views.py
+++ led/views.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+import RPi.GPIO as GPIO
+
+LED_PIN = 7
+
+def turnOn(request) {
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.output(LED_PIN, 1)
+    return HttpResponse('')
+}
+
+def turnOff(request) {
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.output(LED_PIN, 0)
+    Return HttpResponse('')
+}
