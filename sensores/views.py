2b94638809d094c903215ea9160e3b9e0e8ccb17
Tomas Vargas Vicencio <tvargas.vicencio@gmail.com>
2018-07-12 22:07:57 -0400

    Avances en medicion dht11

--- sensores/views.py
+++ sensores/views.py
@@ -8,5 +8,14 @@ import RPi.GPIO as GPIO
 from utils import temhum
 
 def temperatura_humedad(request):
-    hola = temhum.read_dht11_dat()
-    return HttpResponse(''+hola)
+    dato = temhum.read_dht11_dat(8) # conectado a BOARD 8
+    intentos = 1
+    while dato == False:
+        dato = temhum.read_dht11_dat(8)
+        intentos += 1
+    json_response = {
+        "temperatura":dato["temperatura"],
+        "humedad":dato["humedad"]
+        "intentos":intentos,
+    }
+    return HttpResponse(json.dumps(json_response), content_type="application/json")
--- utils/temhum.py
+++ utils/temhum.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 
-#DHT11 connect to BCM_GPIO14
-DHTPIN = 14
+#DHT11 connect to BOARD 8
+#DHTPIN = 8
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
 MAX_UNCHANGE_COUNT = 100
 
@@ -14,7 +14,7 @@ STATE_DATA_FIRST_PULL_DOWN = 3
 STATE_DATA_PULL_UP = 4
 STATE_DATA_PULL_DOWN = 5
 
-def read_dht11_dat():
+def read_dht11_dat(DHTPIN = 8):
     GPIO.setup(DHTPIN, GPIO.OUT)
     GPIO.output(DHTPIN, GPIO.HIGH)
     time.sleep(0.05)
@@ -72,7 +72,7 @@ def read_dht11_dat():
             else:
                 continue
     if len(lengths) != 40:
-        print "Data not good, skip"
+        #print "Data not good, skip"
         return False
 
     shortest_pull_up = min(lengths)
@@ -87,7 +87,7 @@ def read_dht11_dat():
         if length > halfway:
             bit = 1
         bits.append(bit)
-    print "bits: %s, length: %d" % (bits, len(bits))
+    #print "bits: %s, length: %d" % (bits, len(bits))
     for i in range(0, len(bits)):
         byte = byte << 1
         if (bits[i]):
@@ -97,21 +97,23 @@ def read_dht11_dat():
         if ((i + 1) % 8 == 0):
             the_bytes.append(byte)
             byte = 0
-    print the_bytes
+    #print the_bytes
     checksum = (the_bytes[0] + the_bytes[1] + the_bytes[2] + the_bytes[3]) & 0xFF
     if the_bytes[4] != checksum:
-        print "Data not good, skip"
+        #print "Data not good, skip"
         return False
 
-    return the_bytes[0], the_bytes[2]
+    return {
+        "humedad": the_bytes[0], 
+        "temperatura": the_bytes[2],
+    }
 
 def main():
     print "Raspberry Pi wiringPi DHT11 Temperature test program\n"
     while True:
         result = read_dht11_dat()
         if result:
-            humidity, temperature = result
-            print "humidity: %s %%,  Temperature: %s C" % (humidity, temperature)
+            print "humidity: %s %%,  Temperature: %s C" % (result["humedad"], result["temperatur"])
         time.sleep(1)
 
 def destroy():
